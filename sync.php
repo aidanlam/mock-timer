2ce92d78b0a405b07cdea5a09632eb0d28238b25
Aidan Lam <32441938+aidanlam@users.noreply.github.com>
2019-10-28 15:52:30 -0700

    Delete sync.php

--- sync.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-$clientTime = $_GET["ct"] * 1; //for php 5.2.1 or up: (float)$_GET["ct"];
-$serverTimestamp = round(microtime(true)*1000); // (new DateTime())->getTimestamp();
-$serverClientRequestDiffTime = $serverTimestamp - $clientTime;
-echo "{\"diff\":$serverClientRequestDiffTime,\"serverTimestamp\":$serverTimestamp}";
-?>
